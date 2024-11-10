437efb4ac3b6976e7fcc333058be501cf1d8aed7
minju <alswn1583@naver.com>
2024-11-10 23:52:30 +0900

    Add: [백준] 32284번

--- Minju9187/week3/32284.js
+++ Minju9187/week3/32284.js
@@ -2,7 +2,7 @@ let fs = require("fs");
 // let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 let input = fs.readFileSync("예제.txt").toString().split("\n");
 
-const [M, N] = input[0].split(" ").map(Number);
+const [N, M] = input[0].split(" ").map(Number);
 const [a, b] = input[1].split(" ").map(Number);
 
 const grid = Array.from({ length: N }, () => Array(M).fill(""));
@@ -10,17 +10,17 @@ const grid = Array.from({ length: N }, () => Array(M).fill(""));
 for (let i = 0; i < N; i++) {
   for (let j = 0; j < M; j++) {
     if (i > a) {
+      grid[i][j] = "N";
+    } else if (i < a) {
       grid[i][j] = "S";
     } else if (j > b) {
-      grid[i][j] = "N";
-    } else if (j <= b) {
-      grid[i][j] = "E";
-    } else if (j > b) {
       grid[i][j] = "W";
+    } else {
+      grid[i][j] = "E";
     }
   }
 }
 
 for (let row of grid) {
-  console.log(row.join(" "));
+  console.log(row.join(""));
 }
