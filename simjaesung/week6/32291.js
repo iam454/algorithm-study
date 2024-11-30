f5cd305ab0a0fc38e423825dd92cba5424b4ade0
simjaesung <tlawotjd4285@gmail.com>
2024-11-30 12:12:36 +0900

    Add: [백준] 32291번

---
+++ simjaesung/week6/32291.js
@@ -0,0 +1,21 @@
+//K+1의 공약수를 구하면 되는 문제
+let input = require('fs').readFileSync('/dev/stdin').toString().trim();
+let k = +input;
+const ans = [];
+
+const find = (m) => {
+    for(let i = 1; i<=Math.sqrt(m); i++){
+        if(m % i === 0) {
+            ans.push(i);
+            if(i != m/i) ans.push(m/i);
+        }
+    }
+    ans.sort((a,b)=>a-b);
+    ans.pop();
+}
+
+const sol = () => {
+    find(k+1);
+    console.log(ans.join(' '));
+}
+sol();
