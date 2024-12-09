80a1057227ac963d8aeed26d1058bce14e500ed9
fru1tworld <fruitworld.planet@gmail.com>
2024-12-09 18:14:01 +0900

    Add:[LeetCode] 334 문제 해결 및 풀이 추가

--- fru1tworld/leetcode/~500/leet334/leet334.cpp
+++ fru1tworld/leetcode/~500/leet334/leet334.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
-        int min1 = 0x3f3f3f3f; 
-        int min2 = 0x3f3f3f3f;
+        int min1 = 2147483647; 
+        int min2 = 2147483647;
         for(auto x : nums){
             if(x <= min1) min1 = x;
             else if(x <= min2) min2 = x; 
