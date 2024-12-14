e40eaadada12264d5dc9fbfd4a234b1dcc9288e3
fru1tworld <fruitworld.planet@gmail.com>
2024-12-14 14:36:56 +0900

    Add:[Leetcode] 1657 풀이 및 코드

--- fru1tworld/leetcode75/BitManipulation/leet136/leet136.cpp
+++ fru1tworld/leetcode75/BitManipulation/leet136/leet136.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    vector<int> countBits(int n) {
-        vector<int> ans(n+1, 0);
-        for(int i=1; i<=n; i++){
-
-        }
+    int singleNumber(vector<int>& nums) {
+        int ans = 0; 
+        for(auto x : nums){
+            ans ^= x; 
+        }   
         return ans; 
     }
 };
