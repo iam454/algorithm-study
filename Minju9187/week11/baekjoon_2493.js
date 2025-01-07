890fe1b7f35b72a8b6c8fbc3469591b5c3f65211
Minju9187 <alswn1583@naver.com>
2025-01-07 16:06:18 +0900

    Add : [백준] 2493번 탑

--- Minju9187/week11/baekjoon_2493.js
+++ Minju9187/week11/baekjoon_2493.js
@@ -1,10 +1,24 @@
 let fs = require("fs");
-// let input = fs.readFileSync("/dev/stdin").toString().split("\n");
-let input = fs.readFileSync("2.txt").toString().split("\n");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+// let input = fs.readFileSync("2.txt").toString().split("\n");
 
 const N = Number(input.shift());
 
 const tops = input[0].split(" ").map(Number);
 
 const stack = [];
-const result = [];
+const result = new Array(N).fill(0);
+
+for (let i = 0; i < N; i++) {
+  while (stack.length > 0 && stack[stack.length - 1].top < tops[i]) {
+    stack.pop();
+  }
+
+  if (stack.length > 0) {
+    result[i] = stack[stack.length - 1].idx;
+  }
+
+  stack.push({ idx: i + 1, top: tops[i] });
+}
+
+console.log(result.join(" "));
