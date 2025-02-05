9e10415dd8270f1af619943a761e679caf58c9dc
minju <alswn1583@naver.com>
2025-02-06 03:22:07 +0900

    Add : [리트코드] 724번

---
+++ Minju9187/week16/leetcode_724.js
@@ -0,0 +1,16 @@
+var pivotIndex = function (nums) {
+  const sum = nums.reduce((acc, cur) => acc + cur, 0);
+
+  let left = 0;
+  let right = sum - nums[0];
+
+  if (left === right) return 0;
+
+  for (let i = 1; i < nums.length; i++) {
+    left += nums[i - 1];
+    right -= nums[i];
+    if (left === right) return i;
+  }
+
+  return -1;
+};
