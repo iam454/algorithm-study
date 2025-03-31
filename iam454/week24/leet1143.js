a80a11e97f0d30b5e72f337287efd5ae6da67f4a
iam454 <realseogy@gmail.com>
2025-03-31 14:16:27 +0900

    Add: [leetcode] 1143번

---
+++ iam454/week24/leet1143.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+var longestCommonSubsequence = function (text1, text2) {
+  const n = text1.length;
+  const m = text2.length;
+  const memo = Array.from({ length: n + 1 }, () => new Array(m + 1).fill(0));
+
+  for (let i = 1; i < n + 1; i++) {
+    for (let j = 1; j < m + 1; j++) {
+      if (text1[i - 1] === text2[j - 1]) {
+        memo[i][j] = memo[i - 1][j - 1] + 1;
+      } else {
+        memo[i][j] = Math.max(memo[i - 1][j], memo[i][j - 1]);
+      }
+    }
+  }
+
+  return memo[n][m];
+};
