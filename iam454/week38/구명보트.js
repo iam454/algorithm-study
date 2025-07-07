16207a89257cbdf94f92bc55cf5d0622c08a5e14
iam454 <realseogy@gmail.com>
2025-07-07 17:13:11 +0900

    Add: [프로그래머스] 구명보트

---
+++ iam454/week38/구명보트.js
@@ -0,0 +1,22 @@
+function solution(people, limit) {
+  let answer = 0;
+  people.sort((a, b) => a - b);
+
+  let l = 0;
+  let r = people.length - 1;
+
+  while (l <= r) {
+    const sum = people[l] + people[r];
+
+    if (sum > limit) {
+      r -= 1;
+      answer += 1;
+    } else {
+      l += 1;
+      r -= 1;
+      answer += 1;
+    }
+  }
+
+  return answer;
+}
